3ffebd4832621760f8aae27b7f3ccf73e2323d1d
John Vivian <jtvivian@gmail.com>
2015-09-17 21:33:03 -0700

    Overhaul GATK tool
    Removed unnecessary installation of default-jdk (thanks @heuermh)
    
    Replaced COPY with ADD
    
    files now in /opt/gatk/
    
    Ownership of output files now includes GROUP
    
    Invocation of tool now presents help menu if no parameters are passed
    in.

--- gatk/runtime/wrapper.sh
+++ gatk/runtime/wrapper.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Call tool with parameters
-java $JAVA_OPTS -jar /opt/cgl-docker-lib/gatk.jar "$@"
+java $JAVA_OPTS -jar /opt/gatk/gatk.jar "$@"
 # Fix ownership of output files
 UID=$(stat -c '%u' /data)
-chown -R $UID /data
+GID=$(stat -c '%g' /data)
+chown -R $UID:$GID /data
