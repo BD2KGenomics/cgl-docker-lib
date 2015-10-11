6dd361f28a83bb369c4bc5c8ed21d052f0c87abd
John Vivian <jtvivian@gmail.com>
2015-10-10 18:06:19 -0700

    Adding RNA-SeQC

---
+++ rnaseqc/wrapper.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Call tool with parameters
+java $JAVA_OPTS -jar /opt/rnaseqc/RNA-SeQC_v1.1.8.jar "$@"
+# Fix ownership of output files
+UID=$(stat -c '%u' /data)
+GID=$(stat -c '%g' /data)
+chown -R $UID:$GID /data
+
