c295225e7ac18080df102c35d9bd48e4781010c4
John Vivian <jtvivian@gmail.com>
2015-11-02 21:15:45 -0800

    Fix jar invocation

--- mutect/runtime/wrapper.sh
+++ mutect/runtime/wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Call tool with parameters
-java $JAVA_OPTS /opt/mutect/mutect-1.1.7.jar "$@"
+java $JAVA_OPTS -jar /opt/mutect/mutect-1.1.7.jar "$@"
 # Fix ownership of output files
 UID=$(stat -c '%u:%g' /data)
 chown -R $UID /data
