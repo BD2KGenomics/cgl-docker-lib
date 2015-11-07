02806964cdf74bf5c39411b236b4c4e36d026843
John Vivian <jtvivian@gmail.com>
2015-11-06 23:58:55 -0800

    Fix parameter append

--- ubu/wrapper.sh
+++ ubu/wrapper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Call tool with parameters
-java $JAVA_OPTS -jar /opt/ubu/ubu.jar
+java $JAVA_OPTS -jar /opt/ubu/ubu.jar "$@"
 # Fix ownership of output files
 UID=$(stat -c '%u:%g' /data)
 chown -R $UID /data
