f4969277b17d1d26783e13c6423cb4c23fd32b6d
Hannes Schmidt <hannes@ucsc.edu>
2016-02-18 10:03:25 -0800

    rnaseqc: Switch permission fix in shell wrapper to use shell trap

--- rnaseqc/wrapper.sh
+++ rnaseqc/wrapper.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+
+# Fix ownership of output files
+finish() {
+    # Fix ownership of output files
+    user_id=$(stat -c '%u:%g' /data)
+    chown -R ${user_id} /data
+}
+trap finish EXIT
+
 # Call tool with parameters
 java $JAVA_OPTS -jar /opt/rnaseqc/RNA-SeQC_v1.1.8.jar "$@"
-# Fix ownership of output files
-UID=$(stat -c '%u:%g' /data)
-chown -R $UID /data
 
