a56fe6889772c1b9b0eba0392914aac46c9e8a68
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-02-17 18:17:15 -0800

    Add trap to bwakit to chown files so they are not owned by root. Resolves #82.

--- bwakit/wrapper.sh
+++ bwakit/wrapper.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Fix ownership of output files
+finish() {
+    # Fix ownership of output files
+    UID=$(stat -c '%u:%g' /data)
+    chown -R $UID /data
+}
+trap finish EXIT
+
 /opt/bwa.kit/run-bwamem $@ | bash
