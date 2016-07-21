58f41db6951bab3e2555d26987f62a4bcdb6af17
Frank Austin Nothaft <fnothaft@alumni.stanford.edu>
2016-07-21 10:03:44 -0700

    Revised ADAM pipeline to not depend on Docker socket (resolves #147)
    Depends on https://github.com/BD2KGenomics/toil-scripts/pull/351, which adds
    support for running a "native" ADAM installation. Here, we've modified the
    adam-pipeline container to extend from the adam container, which provides a
    native install of ADAM.

--- adam-pipeline/test.py
+++ adam-pipeline/test.py
@@ -11,7 +11,7 @@ class TestADAMPipeline(unittest.TestCase):
         
         # get working directory
         pwd = os.getcwd()
-        outfile = '%s/test/small.processed.bam' % pwd
+        outfile = '%s/test/outdir/small.processed.bam' % pwd
 
         # check for output file in ./test and clean if necessary
         if os.path.exists(outfile):
@@ -22,16 +22,18 @@ class TestADAMPipeline(unittest.TestCase):
         base = ['docker', 'run']
         args = ['--sample', '/%s/test/small.sam' % pwd,
                 '--known-sites', '/%s/test/small.vcf' % pwd,
+                '--output', '/%s/test/outdir/small.processed.bam' % pwd,
                 '--memory', '1']
-        sock = ['-v', '/var/run/docker.sock:/var/run/docker.sock']
-        mirror = ['-v', '/%s/test:/%s/test' % (pwd, pwd)]
+        mounts = ['-v', '/%s/test/small.sam:/%s/test/small.sam' % (pwd, pwd),
+                  '-v', '/%s/test/small.vcf:/%s/test/small.vcf' % (pwd, pwd),
+                  '-v', '/%s/test/outdir:/%s/test/outdir' % (pwd, pwd),]
 
         # Check base call for help menu
         out = subprocess.check_output(base + tool)
         self.assertTrue('Please see the complete documentation' in out)
 
         # run full command on sample inputs and check for existence of output file
-        subprocess.check_call(base + sock + mirror + tool + args)
+        subprocess.check_call(base + mounts + tool + args)
         self.assertTrue(os.path.exists(outfile))
 
 
