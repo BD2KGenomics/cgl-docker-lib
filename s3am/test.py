fed932897e7fd40f4ec878362e5dd6afe15caaf0
CJ <cketchum@ucsc.edu>
2016-09-22 13:54:31 -0700

    Minor improvements and standardization to match codebase

--- s3am/test.py
+++ s3am/test.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python2.7
-# @author Frank Austin Nothaft fnothaft@berkeley.edu
-import subprocess
-import tempfile
+from subprocess import check_output, CalledProcessError, STDOUT
 import unittest
 
 
 class TestS3AM(unittest.TestCase):
 
     def test_docker_call(self):
-        out, err = check_docker_output(tool='quay.io/ucsc_cgl/s3am:1.0a1')
-        self.assertTrue('usage: s3am [--help] {upload,cancel,verify} ...')
-
-def check_docker_output(tool):
-    command = 'docker run ' + tool
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = process.communicate()
-    return output
+        try:
+            check_output(['docker', 'run','quay.io/ucsc_cgl/s3am'], stderr=STDOUT)
+        except CalledProcessError as e:
+            self.assertTrue('usage: s3am [--help] [--version]' in e.output)
+        else:
+            self.fail('Expected s3am to fail without enough arguments')
 
 if __name__ == '__main__':
     unittest.main()
--- s3am/wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# activate s3am virtualenv
-source /opt/s3am/bin/activate
-
-# run command
-s3am $@
